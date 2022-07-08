6f4897289ce3ccd43e6580fd27d50ff17432dcf6
emorilebo <emorylebo@gmail.com>
2022-07-08 16:12:49 +0100

    HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
-use std:: collections::HashMap;
 
-fn main() {
-    let mut map = HashMap::new();
 
-    map.insert(0, "Hi");
-    map.insert(1, "Hi2");
-    println!("{:?}", map);
-
-    match map.get(&0) {
-        Some(str1) => println!("{}", str1),
-        None => println!("Doesn't exist in map"),
+//None, to indicate failure or lack of value, and
+//Some(value), a tuple struct that wraps a value with type T
+fn divide(dividend: i32, divisor: i32)-> Option<i32>{
+    if dividend % divisor != 0{
+        None
+    }else{
+        Some(dividend/divisor)
     }
-
-    match map.get(&2) {
-        Some(str) => println!("{}", str),
-        _ => println!("Doesn't exist in map"),
-    }
-
-    map.remove(&0);
-    println!("{:?}", map);
 }
-   
+fn main() {
+    let divide1 = divide(4, 2);
+    let divide2 = divide(2, 3);
+
+    println!("{:?} unwraps to {}", divide1, divide1.unwrap());
+    println!("{:?}", divide1);
+}
